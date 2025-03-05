f622571caeed5485162494ec65460db34d01ca8b
Imam Azmi <imamazmi33@gmail.com>
2025-03-05 08:25:20 +0700

    init composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'user/unfold-master',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '93fb05b79eb177a84839cde65934c2ab5998b4b6',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'user/unfold-master' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '93fb05b79eb177a84839cde65934c2ab5998b4b6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
